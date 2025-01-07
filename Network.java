6d124156e0e929672b623a00d97808dfe4be31fc
שירה מאור <shira.maor@post.runi.ac.il>
2025-01-07 22:03:12 +0200

    Task

--- Network.java
+++ Network.java
@@ -69,13 +69,17 @@ public class Network {
      *  If any of the two names is not a user in this network,
      *  or if the "follows" addition failed for some reason, returns false. */
     public boolean addFollowee(String name1, String name2) {
-        if (getUser(name1).equals(getUser(name2))){
+        User user1 = getUser(name1);
+        User user2 = getUser(name2);
+
+        if (user1 == null || user2 == null) {
             return false;
         }
-        if (getUser(name1) == null || getUser(name2) == null) {
+
+        if (user1.equals(user2)) {
             return false;
         }
-        
+
         return getUser(name1).addFollowee(name2);  
     }
     
